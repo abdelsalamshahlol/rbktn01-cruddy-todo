eca63bd935a00eaeb6443de183ae3e35782415f5
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-17 22:04:08 +0100

    delete function updated

--- datastore/index.js
+++ datastore/index.js
@@ -93,14 +93,12 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
+  fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err)=>{
+    if (err) {
+      return callback(new Error(`No item with id: ${id}`));
+    }
     callback();
-  }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
